5ed79243355cbbf98f164d720dff337bee8df255
Zephyru <fernando.ripoli@hotmail.com>
2015-07-03 11:32:17 -0300

    Update TestCondicionConTasaDeCorrecion.java

--- workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionConTasaDeCorrecion.java
+++ workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionConTasaDeCorrecion.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TestPerfilWorker;
 
 import java.util.List;
@@ -11,15 +10,12 @@ import Tarea.Tarea;
 import Worker.Worker;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
-=======
 package TestPerfilWorker
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 
 
  public class TestCondicionConTasaDeCorrecion{
 	 
 	 //variables
-<<<<<<< HEAD
 	 CondicionConTasaDeCorreccion condicion;
 	 List<Tarea> tareas;
 	 Worker mockWorker;
@@ -28,23 +24,18 @@ package TestPerfilWorker
 	 
 	 @Before
 	 public void setUp() {
-=======
 	 CondicionConTasaDeCorrecion condicion;
 	 ArrayList<Tarea> tareas;
 	 
 	 @before
 	 public void setUp() throws exeption{
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 		 mockWorker = mock(Worker.class);
 		 mockTarea1 = mock(Tarea.class);
 		 mockTarea2 = mock(Tarea.class);
 		 tareas.add(mockTarea1); tareas.add(mockTarea2);
-<<<<<<< HEAD
 		 condicion = new CondicionConTasaDeCorreccion(75);
-=======
 		 condicion = new CondicionConTasaDeCorrecion(75);
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
-		 
+
 		 
 	 }
 	 
