b6e9912ed9f2501ea566f0d8a1afd339b6c74e46
Zephyru <fernando.ripoli@hotmail.com>
2015-07-03 11:33:40 -0300

    Update TestCondicionPorIdioma.java

--- workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionPorIdioma.java
+++ workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionPorIdioma.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TestPerfilWorker;
 
 import org.junit.Before;
@@ -9,9 +8,7 @@ import Worker.Worker;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-=======
 package TestPerfilWorker
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 
 
  public class TestCondicionPorIdioma{
@@ -19,7 +16,6 @@ package TestPerfilWorker
 	 //variables
 	CondicionPorIdioma condicion;
 	String idioma;
-<<<<<<< HEAD
 	Worker mockWorker;
 		 
 	 @Before
@@ -27,34 +23,26 @@ package TestPerfilWorker
 		mockWorker = mock(Worker.class);
 		idioma = "Español";
 		condicion = new CondicionPorIdioma("Español");	 
-=======
-	 
+
 	 @before
 	 public void setUp() throws exeption{
 		mockWorker = mock(Worker.class);
 		idioma = "Español";
 		condicion = new TestCondicionPorIdioma(mockProyecto);	 
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 	 }
 	 
 	 
 	 @Test
 	 public void testCreador(){
-<<<<<<< HEAD
 		 assertTrue(condicion.getIdiomas().contains("Español"));
-=======
 		 assertEquals(condicion.getIdioma(), "Español");
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 	 }
 	 
 	 @Test
 	 public void testChequear(){
 		 condicion.chequear(mockWorker);
-<<<<<<< HEAD
 		 verify(mockWorker).getIdiomas();
-=======
 		 verify(mockWorker).getIdioma();
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 	 }
  }
 	 
