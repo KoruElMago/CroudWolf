76f2c108afd4d118307fb96db81042e80a570b89
Zephyru <fernando.ripoli@hotmail.com>
2015-07-03 02:40:48 -0300

    Update CondicionDelWorker.java

--- workspace/TP Integrador CrouWolf/src/PerfilWorker/CondicionDelWorker.java
+++ workspace/TP Integrador CrouWolf/src/PerfilWorker/CondicionDelWorker.java
@@ -1,10 +1,29 @@
 package PerfilWorker;
 
-import Worker.Worker;
-
-public abstract class CondicionDelWorker {
-
+public class PerfilDelWorker {
 	
-	public abstract boolean chequear(Worker w);
+	private List<CondicionDelWorker> condiciones;
 	
+
+	//ver si crearla vacia o ya con una list de condiciones dadas
+	public PerfilDelWorker (List<CondicionDelWorker>) ls){
+		this.setCondiciones(ls);
+	}
+	
+	//Verifica todas las condiciones del worker
+	public Boolean chequearWorker(Worker w){
+		for(CondicionDelWorker c : this.getCondiones()){
+			c.chequar(w);
+		}
+	
+	
+	// getters y setters,
+	
+	public void setCondiciones(List<CondicionDelWorker> ls){
+		condiciones = ls;
+	}
+
+	public List<CondicionDelWorker> getCondiones(){
+		return condiciones;
+	}
 }
