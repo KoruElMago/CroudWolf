0ba4532e3e15bd855a1da3e1f506b4e3fe5c9012
Zephyru <fernando.ripoli@hotmail.com>
2015-07-03 11:33:10 -0300

    Update TestCondicionParticipoEnCiertoProyecto.java

--- workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionParticipoEnCiertoProyecto.java
+++ workspace/TP Integrador CrouWolf/Test/TestPerfilWorker/TestCondicionParticipoEnCiertoProyecto.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package TestPerfilWorker;
 
 import org.junit.Before;
@@ -9,27 +8,22 @@ import static org.mockito.Mockito.*;
 import PerfilWorker.CondicionNoParticipoEnCiertoProyecto;
 import Proyecto.Proyecto;
 import Worker.Worker;
-=======
 package TestPerfilWorker
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 
 
  public class TestCondicionParticipoEnCiertoProyecto{
 	 
 	 //variables
-<<<<<<< HEAD
 	 CondicionNoParticipoEnCiertoProyecto condicion;
 	 Worker mockWorker;
 	 Proyecto mockProyecto;
 	 
 	 @Before
 	 public void setUp() {
-=======
 	 CondicionParticipoEnCiertoProyecto condicion;
 	 
 	 @before
 	 public void setUp() throws exeption{
->>>>>>> b11f049864ad2f3b9b69f7665ea0e7b5f2b04f86
 		mockProyecto = mock(Proyecto.class);
 		mockWorker = mock(Worker.class);
 		condicion = new CondicionNoParticipoEnCiertoProyecto(mockProyecto);	 
