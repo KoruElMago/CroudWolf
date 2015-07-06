8f3ee1735981dedf8f2a7970215345e490430582
Zephyru <fernando.ripoli@hotmail.com>
2015-07-06 19:42:36 -0300

    Update CondicionParticipoEnCiertoProyecto.java

--- workspace/TP Integrador CrouWolf/src/PerfilWorker/CondicionParticipoEnCiertoProyecto.java
+++ workspace/TP Integrador CrouWolf/src/PerfilWorker/CondicionParticipoEnCiertoProyecto.java
@@ -7,10 +7,12 @@ public class CondicionParticipoEnCiertoProyecto extends CondicionDelWorker{
 	
 	private Proyecto proyectoEnElQueTieneQueHaberParticipado;
 	
+	//Constructor
 	public CondicionParticipoEnCiertoProyecto(Proyecto p){
 		this.setProyecto(p);
 	}
 	
+	//Retorna un booleano dependiendo del resultado de chequear la condicion por haber partidipado en cierto proyecto.
 	public boolean chequear(Worker w){
 		return (w.getSubscripciones().contains(this.getProyecto()));
 	}
