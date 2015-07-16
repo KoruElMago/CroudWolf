329d1a6ee66fa02b0dd39f730b7713196e6f3ca4
Zephyru <fernando.ripoli@hotmail.com>
2015-07-16 20:14:18 -0300

    Update TipoMChoiceRespUnica.java

--- src/FeedBack/TipoMChoiceRespUnica.java
+++ src/FeedBack/TipoMChoiceRespUnica.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 public class TipoMChoiceRespUnica extends TipoChoice {
 
+	//Constructor.
 	public TipoMChoiceRespUnica(){
 		this.setRespuestasDadas(new ArrayList<RespuestaCompletable>());
 	}
 	
 	@Override
+	//Implementa el mensaje abstracto dejando solo la respuesta dada por parametro.
 	public void elegirRespuesta(RespuestaCompletable respuesta) {
 		this.getRespuestasDadas().clear();
 		this.getRespuestasDadas().add(respuesta);
