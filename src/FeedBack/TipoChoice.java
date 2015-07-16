4809eec85b4ad69134f821c743b21117dc920511
Zephyru <fernando.ripoli@hotmail.com>
2015-07-16 20:10:14 -0300

    Update TipoChoice.java

--- src/FeedBack/TipoChoice.java
+++ src/FeedBack/TipoChoice.java
@@ -7,9 +7,12 @@ public abstract class TipoChoice {
 	
 	protected List<RespuestaCompletable> respuestasDadas;
 
+	//Constructor
 	public TipoChoice(){
 		this.setRespuestasDadas(new ArrayList<RespuestaCompletable>());
 	}
+	
+	//Valida el feedback.
 	public void validar(FeedBack feedBack){
 		boolean res = false;
 		for(RespuestaCompletable r : this.getRespuestasDadas()){
@@ -19,8 +22,10 @@ public abstract class TipoChoice {
 	feedBack.setEstaValidada(true);
 	}
 
+	//Toma una respuesta que eligio el usuario como parametro, de ahi se verá como se implementara.
 	public abstract void elegirRespuesta(RespuestaCompletable respuesta) ;
 
+	//Getters y setters.
 	public List<RespuestaCompletable> getRespuestasDadas() {
 		return respuestasDadas;
 	}
