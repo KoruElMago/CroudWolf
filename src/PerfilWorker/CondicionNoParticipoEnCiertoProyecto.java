6e9ae5ac514b0485c5dc0082fa00ca76939d9f2b
Zephyru <fernando.ripoli@hotmail.com>
2015-07-17 00:46:04 -0300

    Update CondicionNoParticipoEnCiertoProyecto.java

--- src/PerfilWorker/CondicionNoParticipoEnCiertoProyecto.java
+++ src/PerfilWorker/CondicionNoParticipoEnCiertoProyecto.java
@@ -15,7 +15,7 @@ public class CondicionNoParticipoEnCiertoProyecto extends CondicionDelWorker{
 	
 	//Retorna un booleano dependiendo del resultado de chequear la condicion por no haber participado en un proyecto.
 	public boolean chequear(Worker w){
-		return !(w.getSubscripciones().contains(this.getProyecto()));
+		return !(w.participoEn(this.getProyecto()));
 	}
 	
 	
